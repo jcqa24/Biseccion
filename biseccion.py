eb89101a9dbc1d8cfa6dbb598ce3ee4db73bc210
Juan Carlos Quiroz Aguilar <jcqa24@gmail.com>
2021-03-25 23:58:27 -0600

    Documentacion

--- biseccion.py
+++ biseccion.py
@@ -1,29 +1,43 @@
-#####################################################################
-#   Autor :       Juan Carlos Quiroz Aguialar                       #
-#   Fecha :       02/21                                             #
-#   Descripcion:  Metodo de biseccion                               #
-#                                                                   #
-#####################################################################
+"""
+**   Autor :       Juan Carlos Quiroz Aguilar
+**   Fecha :       02/21
+**   Descripcion:  Metodo de biseccion para encontrar raices de funciones
+**                 no necesariamente lineales
+
+"""
 
 import math as mt
 
 
 
-
-################################################################################
-#  Funcion hacer la biseccion una ves que se encontro un intervalo con raiz    #
-#  Recibe a -> Inicio del intervalo  b-> Fin del intervalo                     #
-#  Devuelve la aproximacion de la raiz                                         #
-################################################################################
-
 def biseccion(a,b):
-    dif = 1
+
+    """
+    Divide la funcion en un intervalo mas pequeño (Biseccion)
+
+    Parameters
+    ----------
+    a : float
+        Limite inferior del intervalo
+    b : float
+        Limite superior del intervalo
+
+
+    Returns
+    -------
+    float
+        Aproximacion a la raiz de la funcion
+    """
+
+    diferencia = 1
+
+    
     #Esta variable controla la precision con que se quiere calcular la raiz
-    #innicialmente es 1x10^(-5) = 0.00005
+    #inicialmente es 1x10^(-5) = 0.00005
     #si se requiere mayor precision se puede modificar este valor
     precision = 1*(10**(-5))
-    #Mientras la diferencia sea menor a la precision seguira en este ciclo
-    while (dif > precision ):
+
+    while (diferencia > precision ):
         #calculamos el valor medio
         c = (a+b)/2
         d = fx(a) * fx(c)
@@ -35,29 +49,54 @@ def biseccion(a,b):
             #si la raiz se encuentra en el segundo intervalo se actualiza el limite inicial al valor medio
             if (d < 0):
                 a = c
-        dif = abs(a - b)
+        diferencia = abs(a - b)
     return (a)
 
 
 
-
-################################################################################
-#  Funcion calcular el valor de x en la funcion donde buscamos la raiz         #
-#  Recibe el punto x donde evaluaremos la funcion                              #
-#  Devuelve el valor de la funcion en el punto x                               #
-################################################################################
-
 def fx(x):
-    #se puede cambiar esta funcion, por ejemplo para usar la funcion cos(x)
-    #return mt.cos(x)
+
+    """
+    Evalua una funcion f(x) en el punto x
+        
+    se puede cambiar esta funcion, por ejemplo para usar la funcion cos(x)
+    return mt.cos(x)
+
+    Parameters
+    ----------
+    x : float
+        Punto donde queremos evaluar la funcion
+
+    Returns
+    -------
+    float
+        Valor de la funcion en el punto x
+    """
+
     return mt.sin(x)
 
-################################################################################
-#  Funcion saber si existe una raiz en el intervalo                            #
-#  Recibe a -> Inicio del intervalo  b-> Fin del intervalo                     #
-#  Devuelve cierto si hay una raiz o falso si no la hay                        #
-################################################################################
+
 def busca_intervalo(a,b):
+    """
+    Decide si hay una raiz en el intervalo (a,b)
+
+    Parameters
+    ----------
+    a : float
+        Limite inferior del intervalo
+    b : float
+        Limite superior del intervalo
+
+
+    Returns
+    -------
+    False
+        Si no existe una raiz en el invervalo
+
+    True
+        Si existe una raiz en el intervalo
+    """
+
     c = fx(a) * fx(b)
     if(c < 0):
         return True
@@ -101,7 +140,7 @@ def main():
         i = round(a + paso,3)
 
 
-    print("Metodos de biseccion terminado, desea imprimir las raices?? \n 1->Si \n 2->No")
+    print("Metodo de biseccion terminado, desea imprimir las raices?? \n 1->Si \n 2->No")
     res = int(input())
     if (res == 1):
         print("Raices -> ",raices)
